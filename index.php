c6665cee82fcbfd2a4ced8a1f80b55db5ed26441
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-11-10 15:21:02 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <!-- Banner and nav -->
+  <section class="banner">
+    <div class="myWidth">
+      <nav>
+        <p class="heading">Open Learner</p>
+        <ul>
+          <li>Home</li>
+          <li>Project</li>
+          <li>Pricing</li>
+          <li>Contact</li>
+        </ul>
+        <div class="loginAndJoin">
+          <p><a href="#">Login</a></p>
+          <button class="primaryBtn">Join US</button>
+        </div>
+      </nav>
+    </div>
+  </section>
+  <!-- Banner and nav -->
+
+  <!-- Banner and nav -->
+  <section></section>
+  <!-- Banner and nav -->
+
+  <!-- Banner and nav -->
+  <section></section>
+  <!-- Banner and nav -->
+
+  <!-- Banner and nav -->
+  <section></section>
+  <!-- Banner and nav -->
+
+  <!-- Banner and nav -->
+  <section></section>
+  <!-- Banner and nav -->
+
+  <!-- Banner and nav -->
+  <section></section>
+  <!-- Banner and nav -->
+
+  <!-- Banner and nav -->
+  <section></section>
+  <!-- Banner and nav -->
+
+  <!-- Banner and nav -->
+  <section></section>
+  <!-- Banner and nav -->
+</body>
+
+</html>
